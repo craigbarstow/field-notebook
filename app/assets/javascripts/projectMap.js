7427a08995f100aadc382e634875f88b6470001a
Craig Barstow <craig.r.barstow@gmail.com>
2015-04-24 14:50:00 -0400

    add search styling

--- app/assets/javascripts/projectMap.js
+++ app/assets/javascripts/projectMap.js
@@ -9,10 +9,10 @@ $(document).ready(function(){
     var mapID = mapDivID.replace("map-", "");
 
     var esriWorldTopo = L.tileLayer('http://server.arcgisonline.com/ArcGIS/re' +
-      'st/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
+      'st/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}' /*,
       {
        attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
-    });
+    }*/);
 
     var map = L.map(document.getElementById(mapDivID));
     esriWorldTopo.addTo(map);
